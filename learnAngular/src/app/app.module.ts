cbf291d60ff263093d41b403c831f9611301123f
nomsasasa <nommygray57@gmail.com>
2023-04-19 15:23:03 +0200

    learnAngular

--- learnAngular/src/app/app.module.ts
+++ learnAngular/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    CartComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
